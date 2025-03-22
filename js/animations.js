01bfbb3a6f5df58022bf55b0d94fb120a449b6a6
odzalpha <audrey.lagunzad@gmail.com>
2025-03-22 22:39:29 +0800

    Add files via upload

---
+++ js/animations.js
@@ -0,0 +1,4 @@
+// GSAP animations
+gsap.from('.hero', {duration: 1, y: -50, opacity: 0});
+gsap.from('section', {duration: 1, opacity: 0, y: 50, stagger: 0.2});
+gsap.from('h2', {duration: 0.5, scale: 0.5, opacity: 0, stagger: 0.1, ease: "back"});
